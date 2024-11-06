8595c22873492f225a831ee722fa27de016a3c8f
muthupandib <muthupandib0067@gmail.com>
2024-11-06 10:27:31 +0000

    resta

--- src/components/DishItem/index.js
+++ src/components/DishItem/index.js
@@ -1,11 +1,13 @@
-import {useState, useContext} from 'react'
-
-import CartContext from '../../context/CartContext'
-
 import './index.css'
 
-const DishItem = ({dishDetails}) => {
+const DishItem = ({
+  dishDetails,
+  cartItems,
+  addItemToCart,
+  removeItemFromCart,
+}) => {
   const {
+    dishId,
     dishName,
     dishType,
     dishPrice,
@@ -17,63 +19,50 @@ const DishItem = ({dishDetails}) => {
     dishAvailability,
   } = dishDetails
 
-  const [quantity, setQuantity] = useState(0)
-  const {addCartItem} = useContext(CartContext)
+  const onIncreaseQuantity = () => addItemToCart(dishDetails)
+  const onDecreaseQuantity = () => removeItemFromCart(dishDetails)
 
-  const onIncreaseQuantity = () => setQuantity(prevState => prevState + 1)
-
-  const onDecreaseQuantity = () =>
-    setQuantity(prevState => (prevState > 0 ? prevState - 1 : 0))
-
-  const onAddItemToCart = () => addCartItem({...dishDetails, quantity})
+  const getQuantity = () => {
+    const cartItem = cartItems.find(item => item.dishId === dishId)
+    return cartItem ? cartItem.quantity : 0
+  }
 
   const renderControllerButton = () => (
-    <div className='controller-container d-flex align-items-center bg-success'>
-      <button className='button' type='button' onClick={onDecreaseQuantity}>
+    <div className="controller-container d-flex align-items-center bg-success">
+      <button className="button" type="button" onClick={onDecreaseQuantity}>
         -
       </button>
-      <p className='quantity'>{quantity}</p>
-      <button className='button' type='button' onClick={onIncreaseQuantity}>
+      <p className="quantity">{getQuantity()}</p>
+      <button className="button" type="button" onClick={onIncreaseQuantity}>
         +
       </button>
     </div>
   )
 
   return (
-    <li className='mb-3 p-3 dish-item-container d-flex'>
+    <li className="mb-3 p-3 dish-item-container d-flex">
       <div
         className={`veg-border ${dishType === 1 ? 'non-veg-border' : ''} me-3`}
       >
         <div className={`veg-round ${dishType === 1 ? 'non-veg-round' : ''}`} />
       </div>
-      <div className='dish-details-container'>
-        <h1 className='dish-name'>{dishName}</h1>
-        <p className='dish-currency-price'>
+      <div className="dish-details-container">
+        <h1 className="dish-name">{dishName}</h1>
+        <p className="dish-currency-price">
           {dishCurrency} {dishPrice}
         </p>
-        <p className='dish-description'>{dishDescription}</p>
+        <p className="dish-description">{dishDescription}</p>
         {dishAvailability && renderControllerButton()}
         {!dishAvailability && (
-          <p className='not-availability-text text-danger'>Not available</p>
+          <p className="not-availability-text text-danger">Not available</p>
         )}
         {addonCat.length !== 0 && (
-          <p className='addon-availability-text mb-0'>
-            Customizations available
-          </p>
-        )}
-        {quantity > 0 && (
-          <button
-            type='button'
-            className='btn btn-outline-primary mt-3'
-            onClick={onAddItemToCart}
-          >
-            ADD TO CART
-          </button>
+          <p className="addon-availability-text">Customizations available</p>
         )}
       </div>
 
-      <p className='dish-calories text-warning'>{dishCalories} calories</p>
-      <img className='dish-image' alt={dishName} src={dishImage} />
+      <p className="dish-calories text-warning">{dishCalories} calories</p>
+      <img className="dish-image" alt={dishName} src={dishImage} />
     </li>
   )
 }
